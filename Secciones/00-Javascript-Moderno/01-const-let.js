ecb310c36e6a8068b87750f7f07d2acc3dbaedb2
Svorex <varezsergio@gmail.com>
2021-06-08 15:45:05 +0200

    Javascript Moderno : variables y constantes

---
+++ Secciones/00-Javascript-Moderno/01-const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+
+const nombre = 'Sergio';
+const apellido = 'Várez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado )
+
+// var No se debe de usar...
+if ( true ) {
+    const nombre = 'Peter';
+    console.log(nombre)
+}
+
+console.log( valorDado );
+
